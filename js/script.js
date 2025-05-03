28e691eca3c483d36c9d53b92b35f522f7585f2d
manishbisht10 <manish.bisht@witds.com>
2025-05-03 13:39:22 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// date picker 
+
+flatpickr("#datepicker", {
+    minDate: "today",
+    dateFormat: "Y-m-d"
+});
+
+//date pickers
+
+flatpickr("#datepickers", {
+    minDate: "today",
+    dateFormat: "Y-m-d"
+});
