bb16f4fdd19903ed898633241865d3fd21f7c934
manishbisht10 <manish.bisht@witds.com>
2025-05-06 12:38:27 +0530

    updated css code

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@ const mobileNav = document.getElementById('mobile-nav');
 hamburgerBtn.addEventListener('click', () => {
     mobileNav.classList.toggle('d-none');
 });
+
+// click the video open popup box 
+const videoSrc = "https://youtu.be/O4Jfs26XwpE?si=dc5s5MrQ3bsY0-e3"; 
+const videoFrame = document.getElementById("videoFrame");
+
+document.getElementById("videoModal").addEventListener("show.bs.modal", () => {
+    videoFrame.src = videoSrc;
+});
+
+document.getElementById("videoModal").addEventListener("hidden.bs.modal", () => {
+    videoFrame.src = "";
+});
